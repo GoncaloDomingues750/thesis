20fd55316949d868e17504c6995be6004406b4c0
GoncaloDomingues750 <up202007914@up.pt>
2025-06-25 02:57:35 +0100

    Extnded user sessions from 30 minutes to 5 hours

--- backend/app/auth/auth_routes.py
+++ backend/app/auth/auth_routes.py
@@ -8,7 +8,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
